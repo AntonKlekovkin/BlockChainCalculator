dc1dba0d5fdda39572aba6d30eecacf26d3a8be8
AntonKlekovkin <klanvlad@mail.ru>
2018-02-05 22:54:38 +0400

    Тесты

---
+++ ConsoleCalc/Calc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    public class Calc
+    {
+        public double sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double sum(double x, double y)
+        {
+            return x + y;
+        }
+        public double mul(double x, double y)
+        {
+            return x * y;
+        }
+        public double div(double x, double y)
+        {
+            return x / y;
+        }
+        public double sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
